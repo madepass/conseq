0283b5b97270a2266b3fe0c2b163b12dabb25e33
madepass <madepass@ucdavis.edu>
2025-05-30 00:54:50 +0200

    working qlearningagent; q-plots;

--- agents.py
+++ agents.py
@@ -10,6 +10,18 @@ from gym import spaces
 from environments import Horizon1
 import math
 from ast import literal_eval
+from scipy.signal import savgol_filter
+
+# %% Hyperparameters (config)
+episodes_per_g = 30
+gs = [0.3, -0.3, 0, 0.1]
+
+temperature = 50
+temperature_decay = 0.90
+
+fig_path = "/media/maikito/mad_mini/consequential_task/figs/"
+
+trials_per_g = episodes_per_g * 2
 
 # %% Functions
 def translate_state(env_state):
@@ -222,7 +234,7 @@ class QLearningAgent:
         learning_rate=0.10,
         discount_factor=1,
         temperature=1.0,
-        decay_rate=0.99,
+        temperature_decay=0.99,
         min_temperature=0.1,
         q_table_options="dict",
         alpha_decay=0.99,
@@ -232,7 +244,7 @@ class QLearningAgent:
         self.alpha_decay_rate = alpha_decay
         self.gamma = discount_factor
         self.temperature = temperature  # τ
-        self.decay_rate = decay_rate  # temperature decay
+        self.decay_rate = temperature_decay  # temperature decay
         self.min_temperature = min_temperature
         self.q_table_options = q_table_options
         self.q_table = self._initialize_q_table()
@@ -244,7 +256,12 @@ class QLearningAgent:
                 0: np.zeros(len(self.actions)),  # trial 1 - low delta
                 1: np.zeros(len(self.actions)),  # trial 1 - medium delta
                 2: np.zeros(len(self.actions)),  # trial 1 - high delta
-                3: np.zeros(len(self.actions)),  # trial 2
+                3: np.zeros(len(self.actions)),  # trial 2 - chose small in 0
+                4: np.zeros(len(self.actions)),  # trial 2 - chose big in 0
+                5: np.zeros(len(self.actions)),  # trial 2 - chose small in 1
+                6: np.zeros(len(self.actions)),  # trial 2 - chose big in 1
+                7: np.zeros(len(self.actions)),  # trial 2 - chose small in 2
+                8: np.zeros(len(self.actions)),  # trial 2 - chose big in 2
             }
         elif q_table == "defaultdict":
             self.q_table = defaultdict(
@@ -270,7 +287,12 @@ class QLearningAgent:
         0: trial 1 - low diff (ie difference between stimuli)
         1: trial 1 - medium diff
         2: trial 1 - high diff
-        3: trial 2
+        3: trial 2 - chose small in 0
+        4: trial 2 - chose big in 0
+        5: trial 2 - chose small in 1
+        6: trial 2 - chose big in 1
+        7: trial 2 - chose small in 2
+        8: trial 2 - chose big in 2
         """
         stimuli = env_state["stimuli"]
         diff = round(max(stimuli) - min(stimuli), 3)
@@ -284,13 +306,27 @@ class QLearningAgent:
             else:
                 raise ("Unknown difficulty value!")
         else:  # second trial
-            agent_state = 3
+            if env_state["state_id"] == 1:  # chose big previously
+                if np.isclose(diff, 0.05):
+                    agent_state = 4
+                elif np.isclose(diff, 0.2):
+                    agent_state = 6
+                elif np.isclose(diff, 0.35):
+                    agent_state = 8
+            elif env_state["state_id"] == 2:  # chose small previously
+                if np.isclose(diff, 0.05):
+                    agent_state = 3
+                elif np.isclose(diff, 0.2):
+                    agent_state = 5
+                elif np.isclose(diff, 0.35):
+                    agent_state = 7
         return agent_state
 
     def get_info(self) -> Dict:
         return {
             "alpha": self.alpha,
             "temperature": self.temperature,
+            "agent_state": self.state,
             "q_table": [list(i) for i in agent.q_table.values()],
         }
 
@@ -305,6 +341,7 @@ class QLearningAgent:
 
     def choose_action(self, state):
         state_id = self._translate_state(state)
+        self.state = state_id
         if state_id in [0, 1, 2]:  # trial 1
             q_values = self.q_table[state_id]
             probs = self._softmax(q_values)
@@ -313,7 +350,7 @@ class QLearningAgent:
             return 1  # big, only logical choice
 
     def update(self, state_id, action, reward, next_state_id):
-        if state_id == 3:  # terminal state
+        if state_id not in [0, 1, 2]:  # terminal state
             best_next = 0
             if self.temperature > self.min_temperature:
                 self.temperature *= self.decay_rate
@@ -326,6 +363,9 @@ class QLearningAgent:
 
     def policy(self, state_id):
         return np.argmax(self.q_table[state_id])
+
+    def set_temperature(self, temperature):
+        self.temperature = temperature
 
 
 def correct_choice_column(env_df):
@@ -354,10 +394,58 @@ def import_agent_info_df(file_path):
     return agent_info_df
 
 
+def get_final_policy_idx(qs):
+    g_block_end_trials = np.arange(
+        episodes_per_g * 2, qs.shape[1] + 1, episodes_per_g * 2
+    )
+    final_policy_idx = np.zeros((qs_plot.shape[0] // 2, len(g_block_end_trials)))
+    for _, i in enumerate(range(0, qs_plot.shape[0], n_actions)):
+        diffs = qs[i] - qs[i + 1]
+        sign_change_idx = np.where(np.sign(diffs[:-1]) != np.sign(diffs[1:]))[0] + 1
+        idx = []
+        for end_trial in g_block_end_trials:
+            _idx = np.where(sign_change_idx < end_trial)[0][-1]
+            idx.append(_idx)
+        final_policy_idx[_, :] = sign_change_idx[idx]
+    return final_policy_idx.astype(int)
+
+
+def compute_greedy_actions(agent_info_df):
+    """
+    Returns list of 0's and 1's.
+    0 indicates choosing small is the greedy action.
+    1 indicates choosing big is the greedy action.
+    """
+    greedy_actions = []
+    for (i, row) in agent_info_df.iterrows():
+        q_values = row["q_table"][row["agent_state"]]
+        greedy_action = 1 if q_values[1] > q_values[0] else 0
+        if q_values[1] == q_values[0]:  # [0, 0] case at beginning
+            greedy_action = np.nan
+        greedy_actions.append(greedy_action)
+    return greedy_actions
+
+
+def compute_action_matches_greedy_column(agent_info_df, env_info_df):
+    action_matches_greedy_column = []
+    for i in range(len(env_info_df)):
+        _ = 1 if env_info_df["action"][i] == agent_info_df["greedy_actions"][i] else 0
+        if np.isnan(agent_info_df["greedy_actions"][i]):
+            _ = np.nan
+        action_matches_greedy_column.append(_)
+    return action_matches_greedy_column
+
+
 # %% Training loop
 # TODO: training loop function, can import to other scripts
-env = Horizon1VC(gs=[0.3], episodes_per_g=500)
-agent = QLearningAgent([0, 1], learning_rate=0.1, temperature=3)
+env = Horizon1VC(gs=gs, episodes_per_g=episodes_per_g)
+agent = QLearningAgent(
+    [0, 1],
+    learning_rate=0.9,
+    temperature=temperature,
+    alpha_decay=1,
+    temperature_decay=temperature_decay,
+)
 
 num_episodes = sum(env._generate_episode_counts())  # Use total episode count
 
@@ -367,6 +455,9 @@ env_state = env.reset()
 while not env.done:
     action = agent.choose_action(env_state)
     next_state_env, reward, done, _, info = env.step(action)
+    if len(env_info_list) > 1:  # not first trial
+        if info["g"] != env_info_list[-1]["g"]:  # if g changed
+            agent.set_temperature(temperature)
     env_info_list.append(info)
     agent_info_list.append(agent.get_info())
 
@@ -389,29 +480,79 @@ env_info_df = pd.read_csv("./output/env_info.csv")
 agent_info_df = import_agent_info_df("./output/agent_info.csv")
 
 # visualize q_values
-agent_info_df["q_table"]
-q1, q2, q3, q4, q5, q6 = (
-    [],
-    [],
-    [],
-    [],
-    [],
-    [],
-)
+n_states, n_actions = np.array(agent_info_df["q_table"][0]).shape
+n_trials = agent_info_df.shape[0]
+qs = np.zeros((n_states, n_actions, n_trials))
 for index, row in agent_info_df.iterrows():
-    q1.append(row["q_table"][0][0])
-    q2.append(row["q_table"][0][1])
-    q3.append(row["q_table"][1][0])
-    q4.append(row["q_table"][1][1])
-    q5.append(row["q_table"][2][0])
-    q6.append(row["q_table"][2][1])
-qs = [q1, q2, q3, q4, q5, q6]
+    qs[:, :, index] = row["q_table"]
 
+# visualize correct
+correct = agent_info_df["correct"][::2]  # trial 1 only
+correct = correct.rolling(window=10).mean()
+
+# greedy
+agent_info_df["greedy_actions"] = compute_greedy_actions(agent_info_df)
+agent_info_df["action_matches_greedy"] = compute_action_matches_greedy_column(
+    agent_info_df, env_info_df
+)
+
+# correct, but only for actions that match greedy
+correct_greedy = agent_info_df["correct"][::2][
+    agent_info_df["action_matches_greedy"] == 1
+]
+correct_greedy = correct.rolling(window=5).mean()
 # %%
-fig = plt.figure(figsize=(10, 5))
-for i, _ in enumerate(qs):
-    plt.plot(_, label=f"q{i+1}")
-ax = plt.gca()
-ax.spines[["right", "top"]].set_visible(False)
-plt.legend()
-plt.show()
+fs = 15
+gs = env_info_df["g"].unique().tolist()
+qs_plot = qs[:3, :, :]
+qs_plot = qs_plot.reshape((qs_plot.shape[0] * qs_plot.shape[1], n_trials))
+final_policy_idx = get_final_policy_idx(qs_plot)
+fig, (ax_top, ax_bottom) = plt.subplots(
+    nrows=2,
+    ncols=1,
+    figsize=(8, 8),
+    sharex=True,
+)
+labels = [f"q{i+1}" for i in range(6)]
+ax_top.plot(qs_plot.T, label=labels)
+for i, g in enumerate(gs):
+    for xi, yi in enumerate(range(0, qs_plot.shape[0], n_actions)):
+        print(xi, yi)
+        ax_top.scatter(
+            final_policy_idx[xi][i],
+            qs_plot[yi, final_policy_idx[xi][i]],
+            marker="x",
+            color="k",
+            s=40,
+            zorder=10,
+        )
+for x in range(trials_per_g, qs_plot.shape[1], trials_per_g):
+    ax_top.vlines(x, 0, np.max(qs_plot), linestyles="dashed", color="k", zorder=1)
+    ax_bottom.vlines(x, 0, 1, linestyles="dashed", color="k", zorder=1)
+for i, x in enumerate(range(episodes_per_g - 1, trials_per_g * len(gs), trials_per_g)):
+    ax_top.text(x, np.max(qs_plot), f"g={gs[i]}", ha="center")
+ax_bottom.plot(correct, color="k")
+ax_bottom.plot(correct_greedy, color="green")
+ax_bottom.hlines(0.5, 0, n_trials, color="k", linestyles="dotted", alpha=0.5)
+ax_bottom.set_ylim((0, 1))
+ax_top.spines[["right", "top"]].set_visible(False)
+ax_bottom.spines[["right", "top"]].set_visible(False)
+plt.xticks(ticks=np.arange(0, trials_per_g * len(gs) + 1, trials_per_g))
+plt.xlabel("trial", fontsize=fs)
+ax_top.set_ylabel("q-value", fontsize=fs)
+ax_bottom.set_ylabel("correct %", fontsize=fs)
+ax_top.legend(
+    loc="lower right",
+    ncol=3,
+    frameon=True,
+    prop={"weight": "bold"},
+    facecolor="white",
+    framealpha=1,
+)
+plt.tight_layout()
+save_name = fig_path + "value_comparison.svg"
+plt.savefig(save_name)
+plt.close("all")
+
+# %% Detect intersections
+# extract greedy action
